4be0de754ab942533abe31266368eed9813a3241
Insaf Gallyamov <0nsaf@mail.ru>
2015-11-27 14:33:02 +0300

    some fixes

--- app/controllers/credit_calculator_controller.rb
+++ app/controllers/credit_calculator_controller.rb
@@ -5,6 +5,12 @@ class CreditCalculatorController < ApplicationController
   end
 
   def calculate
+    @amount = params[:amount]
+    @months = params[:months]
+    @kind = params[:kind]
+    @percent = params[:percent]
+    @round = '%.2f'
+
     @result = Calculator.calculate(params[:amount], params[:months], params[:percent], params[:kind])
     @current_date = Date.current
     render :show
